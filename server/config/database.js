992003e4699648c56f7e1a2cda68c59fe0c07ba0
RobWeekes <157428964+RobWeekes@users.noreply.github.com>
2024-09-25 03:38:12 -0700

    done with sequelize db setup

--- server/config/database.js
+++ server/config/database.js
@@ -6,6 +6,12 @@ module.exports = {
     benchmark: true,
     logQueryParameters: true,
     typeValidation: true,
+    logging: console.log
     // logging: false
+    // A false is commented out here as a reminder that this
+    // functionality can be disabled or overwritten with a
+    // different function if desired, something which may
+    // occasionally be helpful in debugging Express applications
+    // in order to clean up the console.
   },
 };
